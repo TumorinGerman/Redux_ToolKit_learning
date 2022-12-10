0286847ded181995d992594d491213d0cb20278e
German <gtumorin@gmail.com>
2022-12-10 11:49:22 +0100

    1

--- src/slices/usersSlice.js
+++ src/slices/usersSlice.js
@@ -5,25 +5,17 @@ import { createSlice, createEntityAdapter } from "@reduxjs/toolkit";
 
 // BEGIN (write your solution here)
 const usersAdapter = createEntityAdapter();
-
-// По умолчанию: { ids: [], entities: {} }
 const initialState = usersAdapter.getInitialState();
 
 const usersSlice = createSlice({
   name: "users",
   initialState,
   reducers: {
-    addUser: usersAdapter.addOne,
     addUsers: usersAdapter.addMany,
-    // Если нужна дополнительная обработка, то создаем свою функцию
-    removeUser: (state, { payload }) => {
-      // ...
-      // Внутри можем вызвать метод адаптера
-      usersAdapter.removeOne(state, payload);
-    },
-    updateUser: usersAdapter.updateOne,
   },
 });
+
+export const selectors = usersAdapter.getSelectors((state) => state.users);
 export const { actions } = usersSlice;
 export default usersSlice.reducer;
 // END
